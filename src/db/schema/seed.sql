c3d625500554b83acf701d76b7eb50f28e4bff49
HJBowers <HJBowers@gmail.com>
2017-11-13 17:00:47 -0800

    updated db connection, schema, and seed file

---
+++ src/db/schema/seed.sql
@@ -0,0 +1,26 @@
+INSERT INTO albums (title, artist) VALUES
+  ('Malibu', 'Anderson .Paak'),
+  ('A Seat at the Table', 'Solange Knowles'),
+  ('Melodrama', 'Lorde'),
+  ('In Rainbows', 'Radiohead')
+;
+
+INSERT INTO users(email, password, name) VALUES
+  ('a@a.com','a','Jane Doe'),
+  ('b@b.com','a','Bob Black'),
+  ('c@c.com','a','Clark Kent'),
+  ('d@d.com','a','Tony Stark')
+;
+
+INSERT INTO reviews(description, user_id, album_id) VALUES
+  ('London, the capital of England and the United Kingdom.','1','2'),
+  ('SF, the best city ever outside of Oakland.','1','2'),
+  ('New New York','2','4'),
+  ('The new frontier. Or something...','3','3'),
+  ('Oakland is soooo awesome!','1','4'),
+  ('So many less words','2','1'),
+  ('SF stufffff','4','1'),
+  ('New York New York New York New York','4','2'),
+  ('ROCKETS AND SHIPS IN SPACE!!','4','3'),
+  ('Meow meow meow','3','1')
+;
