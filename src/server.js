75a178480ab73681dbb195ce012df80f85644f23
HJBowers <HJBowers@gmail.com>
2017-11-13 16:58:11 -0800

    updated server.js with routes and session

---
+++ src/server.js
@@ -0,0 +1,45 @@
+const env = require('dotenv').config()
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const pgSession = require( 'connect-pg-simple')(session)
+const morgan = require( 'morgan' )
+const routes = require('./db/routes')
+
+const port = process.env.PORT
+
+const app = express()
+
+require('ejs')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(morgan('tiny'));
+
+app.use(session({
+  name: process.env.KEY,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: new pgSession({
+    conString: process.env.DATABASE_URL
+  }),
+  cookie: {
+    secure: false,
+    maxAge: 3600000
+  }
+}))
+
+app.use('/', routes)
+
+app.use((req, res) => {
+  res.status(404).render('not_found')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}...`)
+})
